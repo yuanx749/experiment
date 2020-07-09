e8ac89b31491b16cbdce456438c53bb7316bd81c
yuanx749 <yuanx749@gmail.com>
2020-07-09 12:02:11 +0200

    Add sbatch

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/bin/bash -l
+
+#SBATCH -A [project name]
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 1-00:00:00
+#SBATCH -o "./slurm/slurm-%j.out"
 
 log_file=./logs/$(date +%Y%m%d-%H%M%S).log
 python -u ${1} 2>&1 | tee $log_file
